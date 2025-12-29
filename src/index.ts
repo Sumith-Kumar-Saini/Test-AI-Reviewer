11917ed4f6df569fd7478da615881bd67bd671d9
Sumith Kumar Saini <sumithsaini01@gmail.com>
2025-12-29 22:15:50 +0530

    feat: add initial HTML structure and script integration

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,26 @@ import "dotenv/config";
 import express, { Request, Response } from "express";
 import morgan from "morgan";
 import reviewerRouter from "./routers/reviewer";
+import path from "path";
 
-const { log, clear, error } = console;
+const { log, error } = console;
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(morgan("dev"));
 
+app.use(express.static(path.join(__dirname, "../public")));
+
 app.get("/", async (req: Request, res: Response) => {
-  res.json({ message: "hello world" });
+  res.sendFile(path.join(__dirname, "../view", "index.html"));
 });
 
 app.use("/service", reviewerRouter);
 
 app.listen(PORT, function (err) {
   if (err) {
-    console.error(err);
+    error(err);
     process.exit(1);
   }
   log(`Server is listening on port ${PORT}`);
